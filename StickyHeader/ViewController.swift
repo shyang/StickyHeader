c2107dfeb0ac1768424b4163fe212ce02b7011b6
syang <syang@local>
2020-02-16 15:37:59 +0800

    参照物

--- StickyHeader/ViewController.swift
+++ StickyHeader/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "next", style: .plain, target: self, action: #selector(onRight))
+
+        view.backgroundColor = .white
+        _ = UILabel().then { v in
+            v.text = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+            v.numberOfLines = 0
+
+            view.addSubview(v)
+            v.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+        }
     }
 
     @objc func onRight() {
